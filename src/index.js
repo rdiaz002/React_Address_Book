5fa5ebbdd1f8ef88afec06fc83db9f923c0e3867
Ronuel Diaz <diaz.r93@gmail.com>
2019-03-16 15:39:39 -0400

    Added Search Bar

--- src/index.js
+++ src/index.js
@@ -47,65 +47,72 @@ class App extends Component {
         Telephone: "200-707-8670"
       }
     ],
-    "fFirstName":'',
-    "fLastName":'',
-    "fBirthday":'',
-    "fTelephone":'',
+    "fFirstName": '',
+    "fLastName": '',
+    "fBirthday": '',
+    "fTelephone": '',
+    "SearchString": ''
   }
-  addToBookHandler=(event)=>{
+  addToBookHandler = (event) => {
     event.preventDefault();
-    let newAddr ={
+    let newAddr = {
       FirstName: this.state.fFirstName,
       LastName: this.state.fLastName,
       Birthday: this.state.fBirthday,
       Telephone: this.state.fTelephone
     };
-    this.setState({addresses:[...this.state.addresses,newAddr]});
-    this.setState({fFirstName:''});
-    this.setState({fLastName:''});
-    this.setState({fBirthday:''});
-    this.setState({fTelephone:''});
+    this.setState({ addresses: [...this.state.addresses, newAddr] });
+    this.setState({ fFirstName: '' });
+    this.setState({ fLastName: '' });
+    this.setState({ fBirthday: '' });
+    this.setState({ fTelephone: '' });
   }
-  deleteFromBookHandler=(FirstName,LastName,Birthday,Telephone,event)=>{
-    let addresses =[...this.state.addresses];
-    let deleteIndex = addresses.findIndex((item)=>item.Birthday==Birthday && item.FirstName==FirstName &&item.Telephone==Telephone && item.LastName==LastName);
-    addresses.splice(deleteIndex,1);
-    this.setState({"addresses":addresses});
+  deleteFromBookHandler = (FirstName, LastName, Birthday, Telephone, event) => {
+    let addresses = [...this.state.addresses];
+    let deleteIndex = addresses.findIndex((item) => item.Birthday == Birthday && item.FirstName == FirstName && item.Telephone == Telephone && item.LastName == LastName);
+    addresses.splice(deleteIndex, 1);
+    this.setState({ "addresses": addresses });
   }
 
   render() {
     return (
       <div className="App">
         <Container>
-          <header classname="App-header text-left"><h1>React Address Book</h1></header>
+          <header classname="App-header text-left">
+            <nav><input id="Searchbar" onChange={(e)=>this.setState({SeachString:e.target.value})} type="text" className="float-right" placeholder="Search for someone"></input>
+            </nav>
+            <h1>React Address Book</h1>
+            
+          </header>
           <AddressList addresses={this.state.addresses}
-                       close={this.deleteFromBookHandler}>         
+            close={this.deleteFromBookHandler}>
+            searchstr={this.state.SearchString}
           </AddressList>
           <Form className="text-left" onSubmit={this.addToBookHandler}>
             <Form.Row>
               <Form.Group as={Col} controlId="formGridFirst">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="first name" 
-                onChange={(e)=> this.setState({fFirstName:e.target.value})}/>
+                <Form.Control type="text" placeholder="first name"
+                  onChange={(e) => this.setState({ fFirstName: e.target.value })} />
               </Form.Group>
 
               <Form.Group as={Col} controlId="formGridLast">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="last name" 
-                onChange={(e)=> this.setState({fLastName:e.target.value})}/>
+                <Form.Control type="text" placeholder="last name"
+                  onChange={(e) => this.setState({ fLastName: e.target.value })} />
               </Form.Group>
             </Form.Row>
 
             <Form.Group controlId="formGridBirthday">
               <Form.Label>Birthday</Form.Label>
               <Form.Control type="date" placeholder="MM-DD-YYYY"
-              onChange={(e)=> this.setState({fBirthday:e.target.value})}/>
+                onChange={(e) => this.setState({ fBirthday: e.target.value })} />
             </Form.Group>
 
             <Form.Group controlId="formGridTelephone">
               <Form.Label>Telephone</Form.Label>
-              <Form.Control type="tel" placeholder="###-###-####" 
-              onChange={(e)=> this.setState({fTelephone:e.target.value})}/>
+              <Form.Control type="tel" placeholder="###-###-####"
+                onChange={(e) => this.setState({ fTelephone: e.target.value })} />
             </Form.Group>
 
             <Button variant="primary" type="submit">
