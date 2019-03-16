68c1b1c6ab42b789ccaa7f24460416f8502a8753
Ronuel Diaz <diaz.r93@gmail.com>
2019-03-16 13:11:39 -0400

    added default entries

--- src/index.js
+++ src/index.js
@@ -8,7 +8,44 @@ import Col from 'react-bootstrap/Col';
 
 class App extends Component {
   state = {
-    "addresses": []
+    "addresses": [
+      {
+        FirstName: "Cathy",
+        LastName: "Pierce",
+        Birthday: "9/14/1996",
+        Telephone: "200-910-8132"
+      },
+      {
+        FirstName: "Alfonso",
+        LastName: "Cooley",
+        Birthday: "8/10/1973",
+        Telephone: "200-657-9362"
+      },
+      {
+        FirstName: "Victor",
+        LastName: "Gordon",
+        Birthday: "8/3/1970",
+        Telephone: "200-661-9407"
+      },
+      {
+        FirstName: "Colleen",
+        LastName: "Wright",
+        Birthday: "10/28/1967",
+        Telephone: "200-250-7949"
+      },
+      {
+        FirstName: "James",
+        LastName: "Johnston",
+        Birthday: "5/11/1972",
+        Telephone: "200-645-3176"
+      },
+      {
+        FirstName: "Anna",
+        LastName: "Reyes",
+        Birthday: "9/10/1975",
+        Telephone: "200-707-8670"
+      }
+    ]
   }
 
   render() {
@@ -63,7 +100,7 @@ class App extends Component {
               Add to Address Book
             </Button>
           </Form>
-          
+
         </Container>
       </div>
     );
